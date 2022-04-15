bd8543dcdc8c11a3a9552c37125e0caa2fd3e424
Kelig MARTIN <kmartin@wedigital.garden>
2022-04-15 10:49:46 +0200

    change recipe logic

--- src/main/java/model/user/User.java
+++ src/main/java/model/user/User.java
@@ -13,29 +13,29 @@ import java.util.Optional;
 @Builder
 @Getter
 public class User {
-        private Integer id;
-        private String username;
-        private String password;
-        private List<DietaryRegimes> dietaryRegimes;
-        private Map<LocalDate, Integer> caloriesConsumedByDate;
+    private Integer id;
+    private String username;
+    private String password;
+    private List<DietaryRegimes> dietaryRegimes;
+    private Map<LocalDate, Integer> caloriesConsumedByDate;
 
-        public Optional<Integer> getCaloriesByDate(LocalDate date) {
-                return Optional.ofNullable(this.caloriesConsumedByDate.get(date));
-        }
+    public Optional<Integer> getCaloriesByDate(LocalDate date) {
+        return Optional.ofNullable(this.caloriesConsumedByDate.get(date));
+    }
 
-        public boolean isCaloriesOfTheDayValid(LocalDate date) throws CalorieValueNotFoundException {
-                Optional<Integer> calories = this.getCaloriesByDate(date);
-                if (calories.isEmpty()){
-                        throw new CalorieValueNotFoundException("This user has no calories for this day");
-                }
-                return isCaloriesBetweenThresholds(calories.get());
+    public boolean isCaloriesOfTheDayValid(LocalDate date) throws CalorieValueNotFoundException {
+        Optional<Integer> calories = this.getCaloriesByDate(date);
+        if (calories.isEmpty()){
+            throw new CalorieValueNotFoundException("This user has no calories for this day");
         }
+        return isCaloriesBetweenThresholds(calories.get());
+    }
 
-        public boolean isCaloriesBetweenThresholds(Integer caloriesValue){
-                return caloriesValue >= 100 && caloriesValue <= 1000;
-        }
+    public boolean isCaloriesBetweenThresholds(Integer caloriesValue){
+        return caloriesValue >= 100 && caloriesValue <= 1000;
+    }
 
-        public boolean canEatRecipe(Recipe recipe) {
-                return recipe.isRecipeCompatibleWithRegimes(this.dietaryRegimes);
-        }
+    public boolean canEatRecipe(Recipe recipe) {
+        return recipe.isRecipeCompatibleWithRegimes(this.dietaryRegimes);
+    }
 }
